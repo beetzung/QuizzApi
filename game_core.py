af8e073d7022b46e272a391e23cebf0ee691de57
Anton Betsun <bee.tzung.jr@gmail.com>
2022-03-23 19:34:41 +0200

    Fix player score

--- game_core.py
+++ game_core.py
@@ -46,7 +46,7 @@ class Game:
         return [(player.name, player.score) for player in self.players.values()]
 
     def get_winner(self):
-        return max(self.players, key=lambda x: x.score)
+        return max(self.players.values(), key=lambda x: x.score)
 
     def begin(self):
         # TODO add questions
